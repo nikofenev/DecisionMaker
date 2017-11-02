29b44ee3b366b44aad8f4235aa793f21deb9d884
Nikolay Fenev <nfenev@madisoncollege.edu>
2017-11-01 19:17:33 -0500

    The webservice returns an answer from the DB

--- src/main/java/edu/matc/decisionMaker/DecisionMaker.java
+++ src/main/java/edu/matc/decisionMaker/DecisionMaker.java
@@ -1,80 +1,44 @@
 package edu.matc.decisionMaker;
 
+import edu.matc.genericDao.GenericServiceImpl;
+import edu.matc.genericDao.HibernateUtil;
+import edu.matc.genericDao.IGenericService;
 import org.apache.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 @Path("/decision")
 public class DecisionMaker {
     private final Logger LOGGER = Logger.getLogger(this.getClass());
+    IGenericService<Answer> answerService;
+
     // The Java method will process HTTP GET requests
     @GET
     @Path("{crude}/{decisive}/{happy}")
     public Response getDecisionParameters(
-            @PathParam("crude") String statusCrudeFilter,
-            @PathParam("decisive") String statusIndecisiveFilter,
-            @PathParam("happy") String statusIrritatedFilter) {
+        @PathParam("crude") String statusCrudeFilter,
+        @PathParam("decisive") String statusIndecisiveFilter,
+        @PathParam("happy") String statusIrritatedFilter) {
 
+        answerService = new GenericServiceImpl<>(
+            Answer.class, HibernateUtil.getSessionFactory());
 
-            boolean isCrudeOff = Boolean.parseBoolean(statusCrudeFilter);
-            boolean isIndecisiveOff = Boolean.parseBoolean(statusIndecisiveFilter);
-            boolean isIrritatedOff = Boolean.parseBoolean(statusIrritatedFilter);
+        Map<String, Object> params = new HashMap<>();
 
-            List<Answer> answers = new ArrayList<>();
+        String sqlQuery = "from decisions where isCrude='" + statusCrudeFilter + "' and isIndecisive='"
+                    + statusIndecisiveFilter + "' and isIrritated='" + statusIrritatedFilter + "'";
 
-        String answer = "This is the initial value. If you are reading this, something went wrong";
+        List<Answer> answer = answerService.query(sqlQuery,params);
 
-        if (isCrudeOff) {
-            if (isIndecisiveOff) {
-                if (isIrritatedOff) {
-                    answer = "all filters are off; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-                if (!isIrritatedOff) {
-                    answer = "Crudeness and Indecisive filters are off, Irritated is on; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-            }
-            if (!isIndecisiveOff) {
-                if (isIrritatedOff) {
-                    answer = "Crudeness is off, Indecisive is on, Irritated is off; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-                if (!isIrritatedOff) {
-                    answer = "Crudeness is off, Indecisive and Irritated are on, Irritated is on; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-            }
-        }
+        Random randomizer = new Random();
+        Answer random = answer.get(randomizer.nextInt(answer.size()));
 
-        if (!isCrudeOff) {
-            if (!isIndecisiveOff) {
-                if (!isIrritatedOff) {
-                    answer = "all filters are on; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-                if (isIrritatedOff) {
-                    answer = "Crudeness and Indecisive filters are on, Irritated is off; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-            }
-            if (isIndecisiveOff) {
-                if (isIrritatedOff) {
-                    answer = "Crudeness is off, Indecisive is off, Irritated is on; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-                if (!isIrritatedOff) {
-                    answer = "Crudeness is off, Indecisive in on and Irritated is off, Irritated is on; the parameters sent are " + statusCrudeFilter + " " + statusIndecisiveFilter + " " + statusIrritatedFilter;
-                }
-            }
-
-        }
-
-
-
-
-
-
-        return Response.status(200).entity(answer).build();
+        return Response.status(200).entity(random.getAnswer()).build();
     }
 
 }
