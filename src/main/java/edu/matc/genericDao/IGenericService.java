2fe94c86b3e8bd6abe7e3b58db90e397b3eff347
Nikolay Fenev <nfenev@madisoncollege.edu>
2017-11-21 12:02:41 -0600

    Update IGenericService.java

--- src/main/java/edu/matc/genericDao/IGenericService.java
+++ src/main/java/edu/matc/genericDao/IGenericService.java
@@ -1,1 +1,22 @@
-package edu.matc.genericDao;import java.util.List;/** * The Generic service interface. * * @param <T> the type parameter */public interface IGenericService<T> extends IGenericDAO<T> {    /**     * Gets all.     *     * @return all     */    List<T> getAll();    /**     * Deletes all.     */    void deleteAll();}
+package edu.matc.genericDao;
+import java.util.List;
+
+/**
+ * The Generic service interface.
+ *
+ * @param <T> the type parameter
+ */
+public interface IGenericService<T> extends IGenericDAO<T> {
+
+    /**
+     * Gets all.
+     *
+     * @return all
+     */
+    List<T> getAll();
+
+    /**
+     * Deletes all.
+     */
+    void deleteAll();
+}
